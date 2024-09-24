b378ba64eecdc9715336fde9b8875a95e4dc57d5
Albert Manuel <50751136+albertmanuels@users.noreply.github.com>
2024-09-25 01:22:20 +0700

    chore: Migrate from pages route to app route (#6)
    * chore: Upgrade next version 13.8 to 14.2
    
    * refactor: Refactor folder structure following app route
    
    * chore: Move local images to public

--- src/components/CloudinaryImage/View.tsx
+++ src/components/CloudinaryImage/View.tsx
@@ -1,119 +1,120 @@
+"use client";
 import React, { ComponentPropsWithoutRef, useEffect, useState } from "react";
 import Image from "next/image";
 import { buildUrl } from "cloudinary-build-url";
 import clsx from "clsx";
 
 type CloudinaryImageProps = {
-	publicId: string;
-	className?: string;
-	width: any;
-	height: any;
-	alt: string;
-	mdx?: boolean;
-	aspect?: {
-		width: number;
-		height: number;
-	};
-	noStyle?: boolean;
+  publicId: string;
+  className?: string;
+  width: any;
+  height: any;
+  alt: string;
+  mdx?: boolean;
+  aspect?: {
+    width: number;
+    height: number;
+  };
+  noStyle?: boolean;
 };
 
 const CloudinaryImage = ({
-	className,
-	publicId,
-	width,
-	height,
-	alt,
-	mdx = false,
-	aspect,
-	noStyle = false,
-	style,
+  className,
+  publicId,
+  width,
+  height,
+  alt,
+  mdx = false,
+  aspect,
+  noStyle = false,
+  style,
 }: CloudinaryImageProps & ComponentPropsWithoutRef<"figure">) => {
-	const urlBlurred = buildUrl(publicId, {
-		cloud: {
-			cloudName: "albertmanuel",
-		},
-		transformations: {
-			effect: {
-				name: "blur:1000",
-			},
-			quality: 1,
-			rawTransformation: aspect
-				? `c_fill,ar_${aspect?.width}:${aspect?.height},w_${width}`
-				: undefined,
-		},
-	});
+  const urlBlurred = buildUrl(publicId, {
+    cloud: {
+      cloudName: "albertmanuel",
+    },
+    transformations: {
+      effect: {
+        name: "blur:1000",
+      },
+      quality: 1,
+      rawTransformation: aspect
+        ? `c_fill,ar_${aspect?.width}:${aspect?.height},w_${width}`
+        : undefined,
+    },
+  });
 
-	const url = buildUrl(publicId, {
-		cloud: {
-			cloudName: "albertmanuel",
-		},
-		transformations: {
-			rawTransformation: aspect
-				? `c_fill,ar_${aspect?.width}:${aspect?.height},w_${width}`
-				: undefined,
-		},
-	});
+  const url = buildUrl(publicId, {
+    cloud: {
+      cloudName: "albertmanuel",
+    },
+    transformations: {
+      rawTransformation: aspect
+        ? `c_fill,ar_${aspect?.width}:${aspect?.height},w_${width}`
+        : undefined,
+    },
+  });
 
-	const aspectRatio = aspect ? aspect?.height / aspect?.width : undefined;
+  const aspectRatio = aspect ? aspect?.height / aspect?.width : undefined;
 
-	const RESIZED_MAX_WIDTH = 1000;
-	const resizedToMaxWidth = mdx && +width >= RESIZED_MAX_WIDTH;
+  const RESIZED_MAX_WIDTH = 1000;
+  const resizedToMaxWidth = mdx && +width >= RESIZED_MAX_WIDTH;
 
-	return (
-		<figure
-			className={clsx(className, {
-				"overflow-hidden": !noStyle,
-				"mx-auto w-full": mdx && +width <= 800,
-			})}
-			style={{
-				...(mdx && +width <= 800 ? { maxWidth: width } : {}),
-				...style,
-			}}
-		>
-			<div
-				className="banner-blur"
-				style={{
-					position: "relative",
-					height: 0,
-					paddingTop: aspectRatio
-						? `${aspectRatio * 100}%`
-						: `${(+height / +width) * 100}%`,
-				}}
-			>
-				<style jsx>
-					{`
-						.banner-blur::before {
-							content: "";
-							position: absolute;
-							inset: 0;
-							filter: blur(20px);
-							z-index: 0;
-							background: url(${urlBlurred});
-							background-position: center center;
-							background-size: 100%;
-						}
-					`}
-				</style>
-				<div className="absolute top-0 left-0">
-					<span className="mt-0">
-						<Image
-							className="mt-0"
-							src={url}
-							width={
-								resizedToMaxWidth ? Math.min(+width, RESIZED_MAX_WIDTH) : width
-							}
-							height={
-								resizedToMaxWidth
-									? (RESIZED_MAX_WIDTH * +height) / +width
-									: height
-							}
-							alt={alt}
-						/>
-					</span>
-				</div>
-			</div>
-		</figure>
-	);
+  return (
+    <figure
+      className={clsx(className, {
+        "overflow-hidden": !noStyle,
+        "mx-auto w-full": mdx && +width <= 800,
+      })}
+      style={{
+        ...(mdx && +width <= 800 ? { maxWidth: width } : {}),
+        ...style,
+      }}
+    >
+      <div
+        className="banner-blur"
+        style={{
+          position: "relative",
+          height: 0,
+          paddingTop: aspectRatio
+            ? `${aspectRatio * 100}%`
+            : `${(+height / +width) * 100}%`,
+        }}
+      >
+        <style jsx>
+          {`
+            .banner-blur::before {
+              content: "";
+              position: absolute;
+              inset: 0;
+              filter: blur(20px);
+              z-index: 0;
+              background: url(${urlBlurred});
+              background-position: center center;
+              background-size: 100%;
+            }
+          `}
+        </style>
+        <div className="absolute top-0 left-0">
+          <span className="mt-0">
+            <Image
+              className="mt-0"
+              src={url}
+              width={
+                resizedToMaxWidth ? Math.min(+width, RESIZED_MAX_WIDTH) : width
+              }
+              height={
+                resizedToMaxWidth
+                  ? (RESIZED_MAX_WIDTH * +height) / +width
+                  : height
+              }
+              alt={alt}
+            />
+          </span>
+        </div>
+      </div>
+    </figure>
+  );
 };
 
 export default CloudinaryImage;
--- src/components/TableOfContents/View.tsx
+++ src/components/TableOfContents/View.tsx
@@ -1,16 +1,17 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
 const TableOfContents = () => {
-	return (
-		<div className="py-1">
-			<aside className="sticky top-20">
-				<h3 className="font-semibold text-xl">Table Of Content</h3>
-				<Link href="#">Heading 1</Link>
-				<Link href="#">Heading 2</Link>
-			</aside>
-		</div>
-	);
+  return (
+    <div className="py-1">
+      <aside className="sticky top-20">
+        <h3 className="text-xl font-semibold">Table Of Content</h3>
+        <Link href="#">Heading 1</Link>
+        <Link href="#">Heading 2</Link>
+      </aside>
+    </div>
+  );
 };
 
 export default TableOfContents;
