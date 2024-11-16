d5e427b81910df35606e805576f982c18c808b58
Albert Manuel <albertmanuels10@gmail.com>
2024-11-16 20:21:09 +0700

    feat: change brand on navbar

--- src/components/Navbar/View.tsx
+++ src/components/Navbar/View.tsx
@@ -3,16 +3,9 @@ import Link from "next/link";
 import { PAGE_LIST } from "./View.constants";
 import ThemeSlider from "../ThemeSlider/View";
 import { usePathname } from "next/navigation";
-import LabsDarkIcon from "public/icons/labs-circle-dark.svg";
-import LabsLightIcon from "public/icons/labs-circle-light.svg";
-import Image from "next/image";
-import { useTheme } from "next-themes";
 
 const Navbar = () => {
   const pathname = usePathname();
-  const { theme } = useTheme();
-
-  const isDarkMode = theme === "dark";
 
   const isActivePath = (path: string) => {
     if (path === "/") {
@@ -32,14 +25,7 @@ const Navbar = () => {
             href="/"
             className="flex items-center gap-3 text-2xl font-semibold text-darkMode dark:text-white"
           >
-            <Image
-              src={isDarkMode ? LabsLightIcon : LabsDarkIcon}
-              width={55}
-              height={55}
-              alt="LabsIcon"
-              className="hover:rotate-[30deg] hover:transition-transform hover:delay-200 delay-200 transition-transform"
-            />
-            Albertlabs
+            Albert Manuel
           </Link>
         </div>
         <div className="flex flex-row items-center gap-5">
