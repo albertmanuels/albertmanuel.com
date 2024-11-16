d9e2c90f6a700348c24ea8b8b2a1dd05997344c1
Albert Manuel <albertmanuels10@gmail.com>
2024-11-16 20:22:20 +0700

    feat: adjust blog card ui and animation

--- src/components/BlogCard/View.tsx
+++ src/components/BlogCard/View.tsx
@@ -6,11 +6,12 @@ import { formatDate } from "@/src/helpers";
 const BlogCard = ({ blog }: { blog: BlogCardProps }) => {
   return (
     <Link
+      prefetch={true}
       href={`${blog.slug}`}
-      className="group w-full relative flex flex-col border border-1 border-primary-300 dark:border-txt-300 hover:border-accent dark:hover:border-accent rounded-md mb-[10px] transition-border ease-in-out delay-50"
+      className="group w-full relative flex flex-col border border-1 border-primary-300 dark:border-gray-500 hover:border-primary-100 rounded-md mb-[10px] hover:-translate-y-1 delay-75"
     >
-      <div className="min-h-[20vh] max-h-[22vh] px-5 py-3">
-        <h2 className="mb-1 text-2xl font-medium transition ease-in text-primary-200 dark:text-txt-300 group-hover:text-accent delay-50">
+      <div className="min-h-[20dvh] px-5 py-3">
+        <h2 className="mb-1 text-2xl font-medium transition ease-in text-primary-200 dark:text-txt-300">
           {blog.title}
         </h2>
         <p className="mb-5 font-light text-md text-primary-200 dark:text-txt-300">
