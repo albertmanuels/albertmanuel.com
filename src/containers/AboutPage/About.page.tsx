fdf8bf3308f240924bfeb21441019a37a1db8548
Albert Manuel <albertmanuels10@gmail.com>
2024-11-17 00:15:48 +0700

    feat: Create about page

--- src/containers/AboutPage/About.page.tsx
+++ src/containers/AboutPage/About.page.tsx
@@ -1,3 +1,5 @@
+import { experiences } from "@/src/constants";
+import Image from "next/image";
 import React from "react";
 
 const AboutPage = () => {
@@ -6,8 +8,63 @@ const AboutPage = () => {
       <h1 className="mb-3 text-5xl font-semibold not-prose text-primary-200 dark:text-txt-300">
         About
       </h1>
-      <hr className="mb-6" />
-      <ul className="grid gap-3 pb-10 sm:grid-cols-3"></ul>
+      <hr className="mb-8" />
+      <div className="flex flex-col mb-8 md:mb-16 md:justify-between md:flex-row gap-x-10">
+        <div className="mx-auto mb-8 md:mb-0 md:w-[40%]">
+          <Image
+            src="/images/albert.jpeg"
+            width={220}
+            height={220}
+            className="rounded-full w-[180px] h-[180px] md:w-[220px] md:h-[220px] border-[1px] border-primary-300 dark:border-transparent"
+            alt="Albert Manuel"
+            loading="lazy"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+        <div className="mb-10">
+          <p className="mb-5 text-lg text-primary-100 dark:text-txt-100">
+            Hi there! I&rsquo;m Albert Manuel Simbolon. With over 3 years of
+            experience in web development, I have been gaining my skills on both
+            technical and non-technical.
+          </p>
+
+          {/* Education background */}
+          <p className="mb-5 text-lg text-primary-100 dark:text-txt-100">
+            Without a formal IT background,
+          </p>
+
+          {/* Purpose of this web */}
+          <p className="text-lg text-primary-100 dark:text-txt-100">
+            In this website,
+          </p>
+        </div>
+      </div>
+      <div>
+        <h3 className="mb-8 text-3xl font-bold border-b-2 text-primary-200 dark:text-txt-300 w-max border-b-subAccent">
+          Work Experiences
+        </h3>
+        {experiences.map((expericence) => (
+          <div
+            key={expericence.company}
+            className="mb-8 dark:text-txt-200 text-primary-200"
+          >
+            <h4 className="text-xl font-semibold text-subAccent">
+              {expericence.title}
+            </h4>
+            <p className="text-lg">{expericence.company}</p>
+            <p className="mb-2 text-gray-500 dark:text-gray-400 text-md">
+              {expericence.date}
+            </p>
+            <ul className="text-lg list-disc">
+              {expericence.responsibilities.map((item, idx) => (
+                <li key={`${idx}-${item}`}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
--- src/layouts/AppLayout/App.layout.tsx
+++ src/layouts/AppLayout/App.layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
           attribute="class"
         >
           <Navbar />
-          <main className="px-10 lg:px-0 md:max-w-[72rem] mx-auto bg-white dark:bg-darkMode pt-10 min-h-mobile md:min-h-desktop">
+          <main className="px-6 lg:px-0 md:max-w-[72rem] mx-auto bg-white dark:bg-darkMode pt-10 min-h-mobile md:min-h-desktop">
             {children}
           </main>
           <Footer />
