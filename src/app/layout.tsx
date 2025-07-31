e23fb8c0b07bb5f71f640d6b6ca83fe1ff8adf3f
Albert Manuel <albertmanuels10@gmail.com>
2025-07-31 16:03:03 +0700

    feat: config theme for both light and dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-color-theme="light">
       <body className={`${workSans.variable} antialiased`}>
         <Header />
         <main className="min-h-svh max-w-3xl mx-auto px-4">{children}</main>
