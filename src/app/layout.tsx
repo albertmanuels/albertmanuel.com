ecf0668099bc8510bbfc6790e04322fb000b5960
Albert Manuel <albertmanuels10@gmail.com>
2025-08-01 13:23:12 +0700

    fix: lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,29 +5,29 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 const workSans = Work_Sans({
-  variable: "--font-work-sans",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
+	variable: "--font-work-sans",
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-  title: "Albert Manuel | Software Engineer",
+	title: "Albert Manuel | Software Engineer",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-color-theme="dark">
-      <body className={`${workSans.variable} antialiased`}>
-        <Header />
-        <main className="min-h-svh max-w-3xl mx-auto px-4 pt-20">
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" data-color-theme="dark">
+			<body className={`${workSans.variable} antialiased`}>
+				<Header />
+				<main className="min-h-svh max-w-3xl mx-auto px-4 pt-20">
+					{children}
+				</main>
+				<Footer />
+			</body>
+		</html>
+	);
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
-  return (
-    <footer className="mt-auto py-2">
-      <div className="max-w-3xl mx-auto text-center px-4">
-        <p className="text-sm text-center">
-          © {new Date().getFullYear()} Albert Manuel. All rights reserved.
-        </p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="mt-auto py-2">
+			<div className="max-w-3xl mx-auto text-center px-4">
+				<p className="text-sm text-center">
+					© {new Date().getFullYear()} Albert Manuel. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
