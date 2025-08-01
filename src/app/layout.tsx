6b5b7673e4a3be47a01d4519a0c62432041b7173
Albert Manuel <albertmanuels10@gmail.com>
2025-08-01 14:42:02 +0700

    feat: add scroll to top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,34 +5,37 @@ import { cookies } from "next/headers";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { COLOR_THEME_COOKIE } from "@/constants/global";
+import { Scroll } from "lucide-react";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const workSans = Work_Sans({
-	variable: "--font-work-sans",
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-work-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-	title: "Albert Manuel | Software Engineer",
+  title: "Albert Manuel | Software Engineer",
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const savedTheme = (await cookies()).get(COLOR_THEME_COOKIE);
-	const theme = savedTheme?.value || "light";
+  const savedTheme = (await cookies()).get(COLOR_THEME_COOKIE);
+  const theme = savedTheme?.value || "light";
 
-	return (
-		<html lang="en" data-color-theme={theme}>
-			<body className={`${workSans.variable} antialiased`}>
-				<Header initialTheme={theme} />
-				<main className="min-h-svh max-w-3xl mx-auto px-4 pt-20">
-					{children}
-				</main>
-				<Footer />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" data-color-theme={theme}>
+      <body className={`${workSans.variable} antialiased`}>
+        <ScrollToTop />
+        <Header initialTheme={theme} />
+        <main className="min-h-svh max-w-3xl mx-auto px-4 pt-20">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
