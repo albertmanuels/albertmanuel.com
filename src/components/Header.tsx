80322e9fde7073ccb28e99fd825e6943e12ef572
Albert Manuel <albertmanuels10@gmail.com>
2025-08-01 13:17:57 +0700

    feat: refine header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,49 +5,53 @@ import navigationLinks from "@/constants/navigation";
 import { cn } from "@/helpers/utils";
 
 const Header = () => {
-	const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-	useEffect(() => {
-		if (window.screenY > 50) {
-			setIsScrolled(true);
-		}
+  useEffect(() => {
+    if (window.screenY > 50) {
+      setIsScrolled(true);
+    }
 
-		const handleScroll = () => {
-			setIsScrolled(window.scrollY > 0);
-		};
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
 
-		window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	return (
-		<header
-			className={cn(
-				"sticky top-0 right-0 left-0 z-10 isolate transition-colors duration-300 w-full",
-				isScrolled
-					? "backdrop-blur-lg bg-white/20 shadow-sm"
-					: "bg-transparent",
-			)}
-		>
-			<div className={cn("max-w-3xl mx-auto py-4 px-4")}>
-				<nav className="flex justify-between">
-					<Link href="/" className="text-xl font-bold">
-						albertmanuel
-					</Link>
-					<ul className="flex space-x-4">
-						{navigationLinks.map((nav) => (
-							<li key={nav.url}>
-								<Link href={nav.url}>{nav.title}</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-			</div>
-		</header>
-	);
+  return (
+    <header
+      className={cn(
+        "fixed top-0 right-0 left-0 z-10 isolate transition-colors duration-300 w-full px-4"
+      )}
+    >
+      <div
+        className={cn(
+          "max-w-3xl mx-auto py-4 px-4 rounded-lg mt-4",
+          isScrolled
+            ? "backdrop-blur-lg bg-white/20 shadow-sm"
+            : "bg-transparent"
+        )}
+      >
+        <nav className="flex justify-between">
+          <Link href="/" className="text-xl font-bold">
+            albertmanuel
+          </Link>
+          <ul className="flex space-x-4">
+            {navigationLinks.map((nav) => (
+              <li key={nav.url}>
+                <Link href={nav.url}>{nav.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
